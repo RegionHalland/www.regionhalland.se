b5225e154a5bc34be5dd876c1a698cb9c718577f
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-05 21:19:02 +0100

    Trying out left hand navigation

--- web/app/themes/regionhalland/resources/views/partials/nav-sidebar.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-sidebar.blade.php
@@ -1,7 +1,5 @@
 @if(isset($nav_sidebar) && !empty($nav_sidebar))
-<div class="border border-grey-lightest rounded overflow-hidden">
-	<ul class="js-sidebar-nav list-reset" aria-label="Navigation till undersidor">
+	<ul class="rh-secondary-nav" aria-label="Navigation till undersidor">
 		@each('partials.child-list', $nav_sidebar, 'item')
 	</ul>
-</div>
 @endif
