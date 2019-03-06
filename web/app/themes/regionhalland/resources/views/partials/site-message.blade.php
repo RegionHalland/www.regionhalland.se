0166d4d5da94e6809038ad3e404319f2c79f5a2f
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-06 18:28:57 +0100

    Site message implemented

--- web/app/themes/regionhalland/resources/views/partials/site-message.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/site-message.blade.php
@@ -1,8 +1,17 @@
 @php($myMessage = get_region_halland_acf_site_message())    
 @if($myMessage['show_message'] == true)
-  <div class="center">
-    <span>{!! $myMessage['rubrik'] !!}</span>&nbsp;|&nbsp;
-    <span>{!! $myMessage['meddelande'] !!}</span>&nbsp;|&nbsp;
-    <a href="{{ $myMessage['link_url'] }}" target="{{ $myMessage['link_target'] }}">{{ $myMessage['link_title'] }}</a>
+  <div class="rh-message">
+    <span class="rh-message-icon"></span>
+    <div class="rh-message-text">
+      <h5 class="rh-message-text-title">{!! $myMessage['rubrik'] !!}</h5>
+      <p>Datum - varifrån hämtas det?</p>
+      <p>
+        {!! $myMessage['meddelande'] !!}
+      </p>
+      <p>
+        <a class="rh-message-text-link" href="{{ $myMessage['link_url'] }}" target="{{ $myMessage['link_target'] }}">{{ $myMessage['link_title'] }}</a>
+      </p>
+    </div>
   </div>
 @endif
+
