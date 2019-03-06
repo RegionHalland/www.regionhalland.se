00e8edc190989c7e968782421ca835d981dcdc26
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-06 11:03:55 +0100

    Cleaning away tailwind from cookie notice + added rh-button

--- web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
@@ -1,21 +1,12 @@
 @php($checkCookieNotice = check_region_halland_cookie_notice())
 @if ($checkCookieNotice == false)
 	@php($cookie_notice = get_region_halland_cookie_notice())	
-	<div id="cookie-notice" class="relative bg-blue-light py-4 z-4">
-		<div class="container mx-auto">
-			<div class="-mx-4">
-				<div class="cookie-notice__container px-4">
-					<div class="cookie-notice__text-container block md:inline-flex px-4">
-						<svg class="block h-4 w-4 mr-2 align-middle mb-4 md:mb-0">
-							<use xlink:href="#info"/>
-						</svg>
-						<span class="h5">{!! $cookie_notice['message'] !!}</span>
-					</div>
-					<div class="cookie-notice__btn-container px-4 mt-4 md:mt-0">
-						<button id="cookie-consent" class="px-4 py-2 bg-yellow rounded">{!! $cookie_notice['button'] !!}</button>
-					</div>
-				</div>
-			</div>
+	<div id="cookie-notice" class="cookie-notice__container">
+		<div class="cookie-notice__text-container">
+			{!! $cookie_notice['message'] !!}
+		</div>
+		<div class="cookie-notice__btn-container">
+			<button id="cookie-consent" class="rh-button rh-button--primary">{!! $cookie_notice['button'] !!}</button>
 		</div>
 	</div>
 @endif
