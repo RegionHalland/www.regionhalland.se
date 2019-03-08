d42c08c00d880544153bddbce5f815d56a996be2
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-08 09:36:32 +0100

    Accessibility improvements for nav to child pages in section-nav blade

--- web/app/themes/regionhalland/resources/views/partials/section-navigation.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/section-navigation.blade.php
@@ -1,20 +1,22 @@
-<ul class="flex flex-wrap pt3 mx4" aria-labelledby="Länklista" style="background-color: #F4F4F4">
-	@php($page_children = get_region_halland_page_children())
-	@foreach($page_children as $index => $page)
-		<li class="rh-navigation-card col-12 md-col-4 lg-col-4 mx3">
-			<div class="rh-navigation-card-title">
-				<a href="{{ $page->url }}" class="rh-navigation-card-title-link">
-					{{ $page->post_title }}
-				</a>
-				<span class="rh-navigation-card-title-icon"></span>
-			</div>
-			<p class="rh-navigation-card-description">
-				@if(has_excerpt($page->ID)) 
-					{{ $page->acf_excerpt }}
-				@else
-					{{ html_entity_decode(wp_trim_words(region_halland_remove_shortcode($page->post_content), 10, '...'))  }}
-				@endif
-			</p>
-		</li>
-	@endforeach
-</ul>
+<nav aria-label="Undersidor">
+    <ul class="flex flex-wrap pt3 mx4" aria-label="Undersidor" style="background-color: #F4F4F4">
+        @php($page_children = get_region_halland_page_children())
+        @foreach($page_children as $index => $page)
+            <li class="rh-navigation-card col-12 md-col-4 lg-col-4 mx3">
+                <div class="rh-navigation-card-title">
+                    <a href="{{ $page->url }}" class="rh-navigation-card-title-link">
+                        {{ $page->post_title }}
+                    </a>
+                    <span class="rh-navigation-card-title-icon"></span>
+                </div>
+                <p class="rh-navigation-card-description">
+                    @if(has_excerpt($page->ID))
+                        {{ $page->acf_excerpt }}
+                    @else
+                        {{ html_entity_decode(wp_trim_words(region_halland_remove_shortcode($page->post_content), 10, '...'))  }}
+                    @endif
+                </p>
+            </li>
+        @endforeach
+    </ul>
+</nav>
