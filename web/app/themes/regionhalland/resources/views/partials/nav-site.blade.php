2044b54dd3a6b30071d25f1aa91eb1e3a38591db
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-08 10:05:35 +0100

    Utility top links added to new main nav

--- web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
@@ -4,7 +4,24 @@
             <a href="/"><img class="rh-logo-sitename-logo" src="/styleguide2.1.0/images/components/rh-logo-sitename/navigation_logo.svg" alt="Länk till förstasidan"></a>
         </div>
     </div>
-
+    {{-- Utilities --}}
+    <div class="rh-toplinks">
+        <ul class="rh-toplinks-nav">
+            <li class="rh-toplinks-nav-item">
+                <i class="feather icon-alert-triangle rh-toplinks-nav-link-icon"></i>
+                <a class="rh-toplinks-nav-link" href="#">Driftinformation</a>
+            </li>
+            <li class="rh-toplinks-nav-item">
+                <i class="feather icon-headphones rh-toplinks-nav-link-icon"></i>
+                <a class="rh-toplinks-nav-link" href="#">Talande webb</a>
+            </li>
+            <li class="rh-toplinks-nav-item">
+                <i class="feather icon-log-in rh-toplinks-nav-link-icon"></i>
+                <a class="rh-toplinks-nav-link" href="#">E-tjänster</a>
+            </li>
+        </ul>
+    </div>
+    {{-- Utilities END --}}
     <div class="rh-header-search-desktop">
         <div class="rh-search-field">
             <input type="text" class="rh-search-term" placeholder="Placeholder">
