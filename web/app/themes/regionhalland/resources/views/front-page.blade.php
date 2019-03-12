9f311b9d4a3fdb83b9ae5e7821ff8fb738957727
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-12 15:30:22 +0100

    Tweaked front page with blurbs

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -26,11 +26,25 @@
             @endforeach
         </ul>
     </nav>
-    {!! the_content() !!}
-    {{ get_region_halland_acf_page_ingress() }}
-	<div>PUFFAR i tredjedelar här - blurb</div>
-<div>PUFFAR i halvbredd här - visit halland + 1177. Blurb?</div>
 
+    @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
+    @if($myBlurbs)
+        <ul class="flex flex-wrap pt4 pb3 mx4" aria-label="Puffar">
+            @foreach ($myBlurbs as $blurbs)
+                <li class="col-12 sm-col-6 md-col-4 lg-col-4 pr3">
+                    <div class="demo">
+                        <div class="rh-block">
+                            <img src="{!! $blurbs['image_url'] !!}">
+                            <div class="rh-text">
+                                <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}">{{ $blurbs['post_title'] }}</a></h3>
+                                <p>{{ $blurbs['post_content'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    @endif
 {{--
 <main id="main">
 	<div class="bg-blue-dark">
