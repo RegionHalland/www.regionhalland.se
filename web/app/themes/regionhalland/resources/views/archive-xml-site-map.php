8931a5fbb01cdf0f783b117463d9fc87ceb5d4e2
rhn014 <Roland.Hyden@RegionHalland.se>
2019-03-12 12:46:14 +0100

    xml-site-map

---
+++ web/app/themes/regionhalland/resources/views/archive-xml-site-map.php
@@ -0,0 +1,39 @@
+<?php 
+    
+    echo '<?xml version="1.0" encoding="UTF-8" ?>';
+    	
+	$args = array(
+		'posts_per_page'   => -1,
+		'offset'           => 0,
+		'cat'              => '',
+		'category_name'    => '',
+		'orderby'          => 'post_title',
+		'order'            => 'DESC',
+		'include'          => '',
+		'exclude'          => '',
+		'meta_key'         => '',
+		'meta_value'       => '',
+		'post_type'        => 'page',
+		'post_mime_type'   => '',
+		'post_parent'      => '',
+		'author'	       => '',
+		'author_name'	   => '',
+		'post_status'      => 'publish',
+		'suppress_filters' => true,
+		'fields'           => ''
+	);
+	$myPages = get_posts($args);
+
+?>	
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php
+	foreach ($myPages as $page) {
+	 	$strPermaLink = get_permalink($page->ID);
+	 	$strPostType = $page->post_type;
+	?>
+	<url>
+      <loc><?=$strPermaLink?></loc>
+      <posttype><?=$strPostType?></posttype>
+   </url>
+<?php } ?>
+</urlset>
