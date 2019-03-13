b65b255052b68c1787f9e3ee687a4fc928c396b3
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-13 14:17:03 +0100

    New bright factory cookie notice

--- web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
@@ -1,12 +1,11 @@
 @php($checkCookieNotice = check_region_halland_cookie_notice())
 @if ($checkCookieNotice == false)
-	@php($cookie_notice = get_region_halland_cookie_notice())	
-	<div id="cookie-notice" class="cookie-notice__container">
-		<div class="cookie-notice__text-container">
-			{!! $cookie_notice['message'] !!}
-		</div>
-		<div class="cookie-notice__btn-container">
-			<button id="cookie-consent" class="rh-button rh-button--primary">{!! $cookie_notice['button'] !!}</button>
-		</div>
+	@php($cookie_notice = get_region_halland_cookie_notice())
+	<div class="rh-cookie">
+		<span class="rh-cookie-icon"></span>
+		<p class="rh-cookie-description">
+            {!! $cookie_notice['message'] !!}
+		</p>
+		<button class="rh-button rh-button--primary cookie-button" aria-label="Acceptera cookies och dölj meddelandet" role="button">{!! $cookie_notice['button'] !!}</button>
 	</div>
 @endif
