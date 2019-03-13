71676cae4fb315c5087fbdb6d0f0271d79ee189b
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-13 14:19:40 +0100

    Added css and js for 2.2.0 styleguide

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -56,3 +56,4 @@
 </div>
 <script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.3.1'></script>
 <script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/digitaliseringbyran.js'></script>
+<script type='text/javascript' src='{!! env('WP_HOME') !!}/styleguide2.2.0/js/components.js'></script>
