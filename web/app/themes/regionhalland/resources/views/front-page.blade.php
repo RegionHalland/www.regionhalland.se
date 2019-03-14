c0ecec76fa17f8cdbe8a738314ee817bae3fd7da
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-14 14:25:18 +0100

    Hunting down scrollbar bug

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -27,24 +27,7 @@
         </ul>
     </nav>
 
-    @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
-    @if($myBlurbs)
-        <ul class="flex flex-wrap pt4 pb3 mx4" aria-label="Puffar">
-            @foreach ($myBlurbs as $blurbs)
-                <li class="col-12 sm-col-6 md-col-4 lg-col-4 pr3">
-                    <div class="demo">
-                        <div class="rh-block">
-                            <img src="{!! $blurbs['image_url'] !!}">
-                            <div class="rh-text">
-                                <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}">{{ $blurbs['post_title'] }}</a></h3>
-                                <p>{{ $blurbs['post_content'] }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            @endforeach
-        </ul>
-    @endif
+    @include('partials.new_blurbs-list')
 {{--
 <main id="main">
 	<div class="bg-blue-dark">
--- web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
+++ web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
@@ -22,24 +22,7 @@
                 </div>
             </div>
 
-			@php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
-			@if(isset($myBlurbs))
-                <ul class="flex flex-wrap pt4 pb3 mx4" aria-label="Puffar">
-                @foreach ($myBlurbs as $blurbs)
-                    <li class="col-12 sm-col-6 md-col-4 lg-col-4 pr3">
-                    <div class="demo">
-                    <div class="rh-block">
-                        <img src="{!! $blurbs['image_url'] !!}">
-                        <div class="rh-text">
-                            <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}">{{ $blurbs['link_title'] }}</a></h3>
-                            <p>{{ $blurbs['post_content'] }}</p>
-                        </div>
-                    </div>
-                    </div>
-                    </li>
-                @endforeach
-                </ul>
-			@endif
+            @include('partials.new_blurbs-list')
 
             @if(function_exists('get_region_halland_page_news_taxonomi_category'))
                 @php($newsitems = get_region_halland_page_news_taxonomi_category())
