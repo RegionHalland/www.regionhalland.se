5e2e779ff83a138e3c222c7eaf9185d39e534058
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-15 12:10:02 +0100

    Breaking out blurbs into a separate component

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -1,5 +1,6 @@
 @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
 @if(isset($myBlurbs))
+    <div>
     <ul class="flex flex-wrap pt4 pb3 mx4" aria-label="Puffar">
         @foreach ($myBlurbs as $blurbs)
             <li class="col-12 sm-col-6 md-col-4 lg-col-4 pr3">
@@ -15,4 +16,5 @@
             </li>
         @endforeach
     </ul>
+    </div>
 @endif
