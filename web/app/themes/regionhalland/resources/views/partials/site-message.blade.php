338d71923dcbc0cb7f7934f695d6e895b9e5c5ea
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-18 21:31:43 +0100

    Tweaking site message

--- web/app/themes/regionhalland/resources/views/partials/site-message.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/site-message.blade.php
@@ -1,14 +1,17 @@
 @php($myMessage = get_region_halland_acf_site_message())    
 @if($myMessage['show_message'] == true)
-    <div class="rh-message center" style="max-width: 1440px">
-        <span class="rh-message-icon"></span>
-        <div class="rh-message-text">
-            <h5 class="rh-message-text-title">{!! $myMessage['rubrik'] !!}</h5>
-            <p>{!! $myMessage['meddelande'] !!}</p>
-            <p>
-                <a class="rh-message-text-link" href="{{ $myMessage['link_url'] }}" target="{{ $myMessage['link_target'] }}">{{ $myMessage['link_title'] }}</a>
-            </p>
-        </div>
+    <div style="background-color: #E3B7BE;">
+    <div class="rh-message mx-auto" style="max-width: 1440px">
+            <span class="rh-message-icon"></span>
+            <div class="rh-message-text">
+                <h5 class="rh-message-text-title">{!! $myMessage['rubrik'] !!}</h5>
+                <p>{!! $myMessage['meddelande'] !!}</p>
+                <p>
+                    <a class="rh-message-text-link" href="{{ $myMessage['link_url'] }}" target="{{ $myMessage['link_target'] }}">{{ $myMessage['link_title'] }}</a>
+                </p>
+            </div>
+
+    </div>
     </div>
 @endif
 
