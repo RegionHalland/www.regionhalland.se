209393e438dc39da5df4d49f7727d0a92702fb29
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-18 21:18:53 +0100

    Breadcrumbs full width

--- web/app/themes/regionhalland/resources/views/partials/breadcrumbs.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/breadcrumbs.blade.php
@@ -1,21 +1,21 @@
 @if(!is_front_page())
 @php($breadcrumbs = get_region_halland_breadcrumbs())	
 @if(isset($breadcrumbs))
-	<div class="center" style="max-width:1440px;">
-	<div class="rh-breadcrum left-align" >
-		<ul class="rh-breadcrumb-group">
-			@foreach ($breadcrumbs as $breadcrumb)
-				<li class="rh-breadcrumb-group-item" @if ($loop->last) aria-current="page" @endif itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
-					@if ($breadcrumb['url'])
-						<a class="rh-breadcrumb-group-link" href="{{ $breadcrumb['url'] }}">{!! $breadcrumb['name'] !!}</a>
-					@else
-						{!! $breadcrumb['name'] !!}
-					@endif
-					<meta itemprop="position" content="{{ $loop->iteration }}">
-				</li>
-			@endforeach
-		</ul>
-	</div>
-	</div>
+
+		<div class="rh-breadcrumbs">
+			<ul class="rh-breadcrumb-group pl4 mx-auto" style="max-width:1440px;">
+				@foreach ($breadcrumbs as $breadcrumb)
+					<li class="rh-breadcrumb-group-item" @if ($loop->last) aria-current="page" @endif itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+						@if ($breadcrumb['url'])
+							<a class="rh-breadcrumb-group-link" href="{{ $breadcrumb['url'] }}">{!! $breadcrumb['name'] !!}</a>
+						@else
+							{!! $breadcrumb['name'] !!}
+						@endif
+						<meta itemprop="position" content="{{ $loop->iteration }}">
+					</li>
+				@endforeach
+			</ul>
+		</div>
+
 @endif
 @endif
