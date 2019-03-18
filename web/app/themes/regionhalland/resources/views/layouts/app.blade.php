8162df2e92c65470df1a9017ac7185b649214de3
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-18 20:58:50 +0100

    Commenting out cookie notice for now since we have no cookies

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 		
 		@include('partials.site-message')
         @include('partials.jump-to-content')
-		@include('partials.cookie-notice')
+		{{--@include('partials.cookie-notice')--}}
 		@include('partials.site-notices')
 
 		@include('partials.header')
