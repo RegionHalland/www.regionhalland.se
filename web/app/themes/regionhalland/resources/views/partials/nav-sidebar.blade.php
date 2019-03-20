af3fc64b3d17d18f30925bca7a5ff30cb7b39c21
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-20 11:12:36 +0100

    Starting up left hand navigation

--- web/app/themes/regionhalland/resources/views/partials/nav-sidebar.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-sidebar.blade.php
@@ -1,5 +1,11 @@
-@if(isset($nav_sidebar) && !empty($nav_sidebar))
-	<ul class="rh-secondary-nav" aria-label="Navigation till undersidor">
-		@each('partials.child-list', $nav_sidebar, 'item')
-	</ul>
+@if(function_exists('get_region_halland_current_page_and_child_pages'))
+	@php($myPages = get_region_halland_current_page_and_child_pages())
+	@if(isset($myPages) && !empty($myPages['page_children']))
+		<span>{{ $myPages['current_page']->post_title }}</span>
+		@if (!empty($myPages['page_children']))
+			@foreach ($myPages['page_children'] as $myChilds)
+				<a href="{{ $myChilds->url }}">{{ $myChilds->post_title }}</a>
+			@endforeach
+		@endif
+	@endif
 @endif
