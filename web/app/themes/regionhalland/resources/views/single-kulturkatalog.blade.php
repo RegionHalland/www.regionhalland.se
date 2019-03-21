7f2820854ff77a85d8c88c5c8e8ec0e862624b17
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-21 15:38:37 +0100

    Single page kulturkatalog got layout

--- web/app/themes/regionhalland/resources/views/single-kulturkatalog.blade.php
+++ web/app/themes/regionhalland/resources/views/single-kulturkatalog.blade.php
@@ -1,59 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="bg-white pt-16 pb-8">
-	<div class="container mx-auto px-4">
-		<div class="w-full mx-auto">
-			<div class="flex flex-wrap -mx-4">
-				<div class="w-full sm:w-full md:w-7/12 px-4">
-					@while(have_posts()) @php(the_post())
-						<article class="">
-							<h1 class="mb-8">{{ get_the_title() }}</h1>
-							<div class="article">
-								<p>{{ the_content() }}</p>
-							</div>
-							@include('partials.author-info')
-						</article>
-					@endwhile
-				</div>
-				<div class="w-full sm:w-full md:w-5/12 px-4 mt-8 md:mt-0">
-					<div class="sticky pin-t">
-						<header class="relative pb-4 block mb-4">
-							<span class="border-b-2 border-yellow text-2xl font-bold text-black pb-2 z-20 relative leading-none">Mer information</span>
-							<hr class="absolute pin-b pin-l w-full h-0 border-b-2 mb-1 border-yellow-light z-10">
-						</header>
-						<ol class="list-reset bg-yellow-light relative rounded border border-grey-lightest overflow-hidden">
-							<li class="px-4 py-4 border-grey-lightest truncate overflow-hidden relative">
-								<span>
-									<strong>Typ:</strong>
-									@php($arrLabels = get_region_halland_acf_page_kulturkatalog_type_labels())	
-									@if(isset($arrLabels))
-										@foreach ($arrLabels as $label)
-											{{ $label['label'] }}, 
-										@endforeach
-									@endif
-								</span>
-							</li>
-							<li class="px-4 py-4 border-grey-lightest truncate overflow-hidden relative">
-								<span><strong>Publik:</strong> {{ get_region_halland_acf_page_kulturkatalog_publik() }}</span>
-							</li>
-							<li class="px-4 py-4 border-grey-lightest truncate overflow-hidden relative">
-								<span><strong>Speltid:</strong> {{ get_region_halland_acf_page_kulturkatalog_speltid() }}</span>
-							</li>
-							<li class="px-4 py-4 border-grey-lightest truncate overflow-hidden relative">
-								<span><strong>Lokal:</strong> {{ get_region_halland_acf_page_kulturkatalog_lokal() }}</span>
-							</li>
-							<li class="px-4 py-4 border-grey-lightest truncate overflow-hidden relative">
-								<span><strong>Turnéperiod:</strong> {{ get_region_halland_acf_page_kulturkatalog_turne_period() }}</span>
-							</li>
-							<li class="px-4 py-4 border-grey-lightest truncate overflow-hidden relative">
-								<span><strong>Pris:</strong> {{ get_region_halland_acf_page_kulturkatalog_pris() }}</span>
-							</li>
-						</ol>
-					</div>
-				</div>
+	<main>
+		<div class="center rh-hero">
+			<img class="rh-image-hero" src="{{ get_the_post_thumbnail_url() }}" alt="">
+			<div class="rh-caption-hero">
+				<h5 class="rh-caption-hero-title">
+					{{ get_the_title() }}
+				</h5>
 			</div>
 		</div>
-	</div>
-</div>
+
+		<div class="center px4 pt3 pb4" style="max-width: 1440px;">
+			<div class="left-align">
+				<div class="">
+					@while(have_posts()) @php(the_post())
+					<article class="clearfix">
+						<div class="col col-12 md-col-9 pr4">
+							<h1 class="">{{ get_the_title() }}</h1>
+							<p>{{ the_content() }}</p>
+							@include('partials.author-info')
+						</div>
+						<div class="col col-12 md-col-3">
+							<div class="" >
+								<h2 class="h2" style="border-bottom: 2px solid #61A2D8;">Mer information</h2>
+								<div class="mt2 pt2 pl2 pb2" style="border-left: 4px solid #61A2D8; background-color: #D0E3F3; border-bottom-left-radius: 5px; border-top-left-radius: 5px;">
+									<p><strong>Publik:</strong> {{ get_region_halland_acf_page_kulturkatalog_publik() }}</p>
+									<p><strong>Speltid:</strong> {{ get_region_halland_acf_page_kulturkatalog_speltid() }}</p>
+									<p><strong>Lokal:</strong> {{ get_region_halland_acf_page_kulturkatalog_lokal() }}</p>
+									<p><strong>Turnéperiod:</strong> {{ get_region_halland_acf_page_kulturkatalog_turne_period() }}</p>
+									<p><strong>Pris:</strong> {{ get_region_halland_acf_page_kulturkatalog_pris() }}</p>
+                                    <p><strong>Typ:</strong>
+                                        @php($arrLabels = get_region_halland_acf_page_kulturkatalog_type_labels())
+                                        @if(isset($arrLabels))
+                                            @foreach ($arrLabels as $label)
+                                                {{ $label['label'] }},
+                                            @endforeach
+                                        @endif
+								    </p>
+								</div>
+							</div>
+						</div>
+
+					</article>
+					@endwhile
+				</div>
+
+			</div>
+		</div>
+	</main>
 @endsection
