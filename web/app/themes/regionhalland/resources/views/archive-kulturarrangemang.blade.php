e7472830c48a6807169bcac201ada0fa358e9732
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-21 13:09:56 +0100

    Basic version of kulturarrangemang overview

--- web/app/themes/regionhalland/resources/views/archive-kulturarrangemang.blade.php
+++ web/app/themes/regionhalland/resources/views/archive-kulturarrangemang.blade.php
@@ -2,25 +2,45 @@
 
 @section('content')
 
-<main class="center" id="main" style="max-width: 1440px;">
-	<div class="left-align px4 pt3 pb4">
-		<h1>Kulturarrangemang</h1>
-		@php($myItems = get_region_halland_acf_page_kulturarrangemang_items())
-		@foreach($myItems as $item)
-			<article>
-				<h2><a href="{{$item->url}}">{{ $item->post_title }}</a></h2><br>
-				<span>{{ $item->post_ingress }}</span><br><br>
-				<span>{!! $item->post_content !!}</span><br><br>
-				<span><strong>Typ:</strong> {{ $item->kultur_typ }}</span><br>
-				<span><strong>Kategori:</strong> {{ $item->kultur_category }}</span><br>
-				<span><strong>Subkategori:</strong> {{ $item->kultur_sub_category }}</span><br>
-				<span><strong>Fullbokat:</strong> {{ $item->kultur_fullbokat }}</span><br>
-				<span><strong>Tid:</strong> {{ $item->kultur_tid }}</span><br>
-				<span><strong>Plats:</strong> {{ $item->kultur_plats }}</span><br>
-				<span><strong>Målgrupp:</strong> {{ $item->kultur_malgrupp }}</span><br><br>
-			</article>
-		@endforeach
-	</div>
+<main class="" id="main">
+	<div class="center px4 pt3 pb2" style="max-width: 1440px;">
+        <h1 class="left-align pb2">
+            Arrangemangskalender
+        </h1>
+    </div>
+    <div style="background-color: #E4E4E4">
+        <div class="center px4 pt4 pb4" style="max-width:1440px">
+            @php($myItems = get_region_halland_acf_page_kulturarrangemang_items())
+            <ul class="left-align" aria-label="Arrangemangskalender">
+                @foreach($myItems as $item)
+                    <li>
+                        <div class="rh-event-card-calendar mb2 l2" style="background-color: white;">
+                            <div class="rh-event-card-calendar-date" style="position: relative; top:50%; transform: translateY(+35%);">
+                                <h4 class="rh-event-card-calendar-date-day">14</h4>
+                                <h6 class="rh-event-card-calendar-date-month">sep</h6>
+                            </div>
+                            <div class="py2 rh-event-card-calendar-description">
+                                <h5 class="rh-event-card-calendar-title">
+                                    <a href="{{ $item->url }}" class="rh-event-card-calendar-title-link">
+                                        {{ $item->post_title }}
+                                    </a>
+                                </h5>
+                                <p class="rh-event-card-calendar-inline rh-event-card-calendar-time">
+                                    {{ $item->kultur_tid }}
+                                </p>
+                                <p class="">
+                                    {{ $item->kultur_plats }}
+                                </p>
+                                <span class="rh-labels">
+                                    {{ $item->kultur_sub_category }}
+                                </span>
+                            </div>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </main>
 
 @endsection
