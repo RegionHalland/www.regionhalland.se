47233570a92eddeb270c08b2aaa4334620a240bd
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-21 07:13:09 +0100

    Green headlines on landing page

--- web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
+++ web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
@@ -41,7 +41,7 @@
                         <ul class="flex flex-wrap pb3 mx4 pt3 px2" aria-label="Nyheter" style="background-color: white; border-radius: 0.4ex; box-shadow: 1px 2px 6px grey;">
                             @foreach($newsitems as $newsitem)
                                 <li class="rh-article pb2 col-12 sm-col-6 md-col-6 lg-col-6 px2">
-                                    <h5 class="rh-article-title"><a class="rh-article-title-link" href="{{ $newsitem['permalink'] }}">{{ $newsitem['title'] }}<a/></h5>
+                                    <h5 class="rh-article-title"><a class="rh-article-title-link" style="color: #378A30;" href="{{ $newsitem['permalink'] }}">{{ $newsitem['title'] }}<a/></h5>
                                     <p class="rh-article-published">Publicerad: {{ $newsitem['date'] }}</p>
                                     <p class="rh-article-description">
                                         {{ wp_trim_words(region_halland_remove_shortcode($newsitem['content']), 20, '...') }}
