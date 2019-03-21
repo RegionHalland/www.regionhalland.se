f62277908ca3136afa1442f44ced291c50fcd6c2
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-21 16:26:01 +0100

    Single news page got a metadata box to the right

--- web/app/themes/regionhalland/resources/views/single-news.blade.php
+++ web/app/themes/regionhalland/resources/views/single-news.blade.php
@@ -9,9 +9,15 @@
 				<div class="">
 					<p>{{ the_content() }}</p>
 				</div>
-				@include('partials.author-info')
+
 			@endwhile
 		</main>
 	</div>
+    <div class="left-align col col-12 md-col-3">
+        <div class="mt2 pt2 pl2 pb2" style="border-left: 4px solid #61A2D8; background-color: #D0E3F3; border-bottom-left-radius: 5px; border-top-left-radius: 5px;">
+            <p><strong>Publicerad:</strong> {{ get_the_date('Y-m-d') }}
+            <p><strong>Senast ändrad:</strong> <time itemprop="dateModified" datetime="{{ the_modified_date('Y-m-d') }}">{{ the_modified_date('Y-m-d') }}</time></p>
+        </div>
+    </div>
 </div>
 @endsection
